76798d5a8bdd630a7b748f2dbe80184c657a4dca
Bella Oleksy <36306783+bellaoleksy@users.noreply.github.com>
2022-07-11 15:42:37 -0600

    re-format script to pull in a new, longer DO and temperature timeseries for The Loch and Sky Pond. Also add wx and chlorophyll data.

--- scripts/00_loadData.R
+++ scripts/00_loadData.R
@@ -41,134 +41,132 @@ sky_littoral <- bind_rows(SB1, SB2, SB3, SB4, SB5) %>%
   mutate(habitat = "littoral")
 loch_littoral <- bind_rows(LB3, LB4, LB5, LB6, LB1) %>%
   mutate(habitat = "littoral")
+
 littoral_master <- bind_rows(sky_littoral,
                              loch_littoral) %>%
   mutate(dateTime = round_date(dateTime, "hour")) #For making joining to pelagic data easier
 
+#' # Sky buoy temperature 2016
+#+ warning = FALSE, message = FALSE, eval=FALSE
+#' sky_buoy_long_original <-
+#'   read.table(here("data/sky/pelagic/sky_2016_tempProfile.txt"),
+#'              sep = ",",
+#'              header = TRUE) %>%
+#'   mutate(
+#'     dateTime = ymd_hms(dateTime),
+#'     dateTime = with_tz(dateTime, tz = "America/Denver"),
+#'     dateTime = force_tz(dateTime, "America/Denver")
+#'   ) %>%
+#'   # filter(dateTime >= "2016-06-13" &
+#'   #          dateTime <= "2016-10-30") %>% #ice off and on dates
+#'   rename(wtr_6.5 = wtr_7.0) %>%
+#'   pivot_longer(-dateTime, names_to = "depth") %>%
+#'   mutate(habitat = "pelagic") %>%
+#'   separate(col = depth,
+#'            into = c("parameter", "depth"),
+#'            sep = "_") %>%
+#'   mutate(parameter = "temperature",
+#'          lakeID = "SkyPond")
+#' #' 
+#' #' #' Updated Sky Pond 0.5m and 6.5m miniDot data from 2016 through 2018
+#' surface <- read_csv(here("data/sky/pelagic/new/sky_0.5m_temp_DO.csv"))%>%
+#'   mutate(dateTime= mdy_hm(dateTime)) %>%
+#'   pivot_longer(temp_0.5:DO_0.5,
+#'                names_to = c("parameter","depth"),
+#'                names_sep = "_",
+#'                values_to = "value")
+#' hypo <- read_csv(here("data/sky/pelagic/new/sky_6.5m_temp_DO.csv"))%>%
+#'   mutate(dateTime= mdy_hm(dateTime)) %>%
+#'   pivot_longer(temp_6.5:DO_6.5,
+#'                names_to = c("parameter","depth"),
+#'                names_sep = "_",
+#'                values_to = "value")
+#' new_sky <- bind_rows(surface, hypo) %>%
+#'   mutate(dateTime = with_tz(dateTime, tz = "America/Denver"),
+#'          dateTime = force_tz(dateTime, "America/Denver"),
+#'          dateTime=round_date(dateTime, "30 min"))
+#' #' 
+#' sky_buoy_long <- new_sky %>%
+#'   bind_rows(., sky_buoy_long_original %>%
+#'               select(-habitat) %>%
+#'               mutate(parameter=recode(parameter, temperature="temp"),
+#'                      dateTime=round_date(dateTime, "30 min"))%>%
+#'               filter(!dateTime %in% new_sky$dateTime))
+#' min(sky_buoy_long$dateTime)
+#' max(sky_buoy_long$dateTime)
+#' write_csv(sky_buoy_long, here("data/sky_buoy_long_temp_DO_2016-07-14_to_2018-08-25.csv"))
 
-
-
-#' # Sky buoy temperature
-#+ warning = FALSE, message = FALSE
-sky_buoy_long <-
-  read.table(here("data/sky/pelagic/sky_2016_tempProfile.txt"),
-             sep = ",",
-             header = TRUE) %>%
-  mutate(
-    dateTime = ymd_hms(as.factor(dateTime)),
-    dateTime = force_tz(dateTime, tz = "America/Denver"),
-    dateTime = with_tz(dateTime, "GMT")
-  ) %>%
-  filter(dateTime >= "2016-06-13" &
-           dateTime <= "2016-10-30") %>% #ice off and on dates
-  rename(wtr_6.5 = wtr_7.0) %>%
-  pivot_longer(-dateTime, names_to = "depth") %>%
-  mutate(habitat = "pelagic") %>%
-  separate(col = depth,
-           into = c("parameter", "depth"),
-           sep = "_") %>%
-  mutate(parameter = "temperature",
-         lakeID = "SkyPond")
+#' Read in Sky Pond buoy data 2016-07-14 20:30:00 UTC through 2018-08-25 09:00:00 UTC
+#' I commented out the wrangling I did above to combine a few .csv files
+sky_buoy_long<-read_csv(here("data/sky_buoy_long_temp_DO_2016-07-14_to_2018-08-25.csv")) %>%
+  mutate(dateTime = with_tz(dateTime, tz = "America/Denver"),
+         dateTime = force_tz(dateTime, "America/Denver"),
+         depth = as.character(as.numeric(depth)))
 
 #' + Note that 0.5 and 6.5 are MiniDOT observations which are more frequent than the others
 #' + All other depths were recorded with iButtons, same as the littoral zone sites
-#' + sky_2016_sensorTemp_6.5m.txt is the temperature only at 6.5m; MiniDOT
-#' + sky_2016_sensorTemp.txt is the temperature only at 0.5m; MiniDOT
 
 
 #' # Loch buoy temperature
 #+ warning = FALSE, message = FALSE
 
-loch_buoy_long <- read.table(here("data/loch/pelagic/loch_2016_tempProfile.txt"), sep=",", header=TRUE) %>%
-  mutate(dateTime = ymd_hms(as.factor(dateTime)),
-         dateTime = force_tz(dateTime, tz="America/Denver"),
-         dateTime = with_tz(dateTime, "GMT")) %>%
-  filter(dateTime > "2016-05-31" & dateTime <= "2016-10-30") %>% #ice off and on dates
-  pivot_longer(-dateTime, names_to="depth") %>%
-  mutate(habitat="pelagic") %>%
-  separate(col = depth, into = c("parameter", "depth"), sep = "_") %>%
-  mutate(parameter="temperature",
-         lakeID="TheLoch")
-#' + Note that 0.5 and 4.5 are MiniDOT observations which are more frequent than the others
-#' + All other depths were recorded with iButtons, same as the littoral zone sites
-#' + loch_2016_sensorTemp_4.5m.txt is the temperature only at 4.5; MiniDOT
-#' + loch_2016_sensorTemp_0.5m.txt is the temperature only at 0.5m; MiniDOT
+#' loch_buoy_long_original <- read.table(here("data/loch/pelagic/loch_2016_tempProfile.txt"), sep=",", header=TRUE) %>%
+#'   mutate(dateTime = ymd_hms(as.factor(dateTime)),
+#'          dateTime = force_tz(dateTime, tz="America/Denver"),
+#'          dateTime = with_tz(dateTime, "America/Denver")) %>%
+#'   filter(dateTime > "2016-05-31" & dateTime <= "2016-10-30") %>% #ice off and on dates
+#'   pivot_longer(-dateTime, names_to="depth") %>%
+#'   mutate(habitat="pelagic") %>%
+#'   separate(col = depth, into = c("parameter", "depth"), sep = "_") %>%
+#'   mutate(parameter="temperature",
+#'          lakeID="TheLoch")
+#' 
+#' #' #' Updated The Loch 0.5m and 4.5m miniDot data from 2016 through 2018
+#' surface <- read_csv(here("data/loch/pelagic/loch_0.5m_temp_DO.csv"))%>%
+#'   mutate(dateTime= mdy_hm(dateTime)) %>%
+#'   pivot_longer(temp_0.5:DO_0.5,
+#'                names_to = c("parameter","depth"),
+#'                names_sep = "_",
+#'                values_to = "value")
+#' hypo <- read_csv(here("data/loch/pelagic/loch_4.5m_temp_DO.csv"))%>%
+#'   mutate(dateTime= mdy_hm(dateTime)) %>%
+#'   pivot_longer(temp_4.5:DO_4.5,
+#'                names_to = c("parameter","depth"),
+#'                names_sep = "_",
+#'                values_to = "value")
+#' new_loch <- bind_rows(surface, hypo) %>%
+#'   mutate(dateTime = with_tz(dateTime, tz = "America/Denver"),
+#'          dateTime = force_tz(dateTime, "America/Denver"),
+#'          dateTime=round_date(dateTime, "30 min"))
+#' #' 
+#' loch_buoy_long <- new_loch %>%
+#'   bind_rows(., loch_buoy_long_original %>%
+#'               select(-habitat) %>%
+#'               mutate(parameter=recode(parameter, temperature="temp"),
+#'                      dateTime=round_date(dateTime, "30 min"))%>%
+#'               filter(!dateTime %in% new_loch$dateTime))
+#' min(loch_buoy_long$dateTime)
+#' max(loch_buoy_long$dateTime)
+#' write_csv(loch_buoy_long, here("data/loch_buoy_long_temp_DO_2016-07-19_to_2018-09-11.csv"))
 
-
-#' # Sky buoy DO
-#+ warning = FALSE, message = FALSE
-sky_DO_0.5 <-
-  read.table(here("data/sky/pelagic/sky_2016_DO_0.5m.txt"),
-             sep = ",",
-             header = TRUE) %>%
-  mutate(
-    dateTime = ymd_hms(dateTime),
-    depth = 0.5,
-    lakeID = "SkyPond",
-    habitat = "pelagic"
-  ) %>%
-  filter(dateTime < "2016-11-02")
-
-sky_DO_6.5 <-
-  read.table(here("data/sky/pelagic/sky_2016_DO_6.5m.txt"),
-             sep = ",",
-             header = TRUE) %>%
-  mutate(
-    dateTime = ymd_hms(dateTime),
-    depth = 6.5,
-    lakeID = "SkyPond",
-    habitat = "pelagic"
-  ) %>%
-  filter(dateTime < "2016-11-02")
-
-
-#' # Loch buoy DO
-#+ warning = FALSE, message = FALSE
-
-loch_DO_0.5 <-
-  read.table(here("data/loch/pelagic/loch_2016_DO_0.5m.txt"),
-             sep = ",",
-             header = TRUE) %>%
-  mutate(
-    dateTime = ymd_hms(dateTime),
-    depth = 0.5,
-    lakeID = "TheLoch",
-    habitat = "pelagic"
-  )
-loch_DO_4.5 <-
-  read.table(here("data/loch/pelagic/loch_2016_DO_4.5m.txt"),
-             sep = ",",
-             header = TRUE) %>%
-  mutate(
-    dateTime = ymd_hms(dateTime),
-    depth = 4.5,
-    lakeID = "TheLoch",
-    habitat = "pelagic"
-  ) 
-
-#' Combine all DO data
-#+ warning = FALSE, message = FALSE
-DO_master <- bind_rows(sky_DO_0.5,
-                       sky_DO_6.5,
-                       loch_DO_0.5,
-                       loch_DO_4.5) %>%
-  rename(value=DO) %>%
-  mutate(parameter="DO",
-         depth=as.character(as.numeric(depth)))
+#' Read in Sky Pond buoy data 2016-07-14 20:30:00 UTC through 2018-08-25 09:00:00 UTC
+#' I commented out the wrangling I did above to combine a few .csv files
+loch_buoy_long<-read_csv(here("data/loch_buoy_long_temp_DO_2016-07-19_to_2018-09-11.csv")) %>%
+  mutate(dateTime = with_tz(dateTime, tz = "America/Denver"),
+         dateTime = force_tz(dateTime, "America/Denver"),
+         depth = as.character(as.numeric(depth)))
 
 #' Master buoy df
 #+ warning = FALSE, message = FALSE
 buoy_master <-
-  bind_rows(DO_master, loch_buoy_long, sky_buoy_long) %>%
+  bind_rows(loch_buoy_long, sky_buoy_long) %>%
   mutate(
     depth_category = case_when(depth == 0.5 ~ "surface",
-                               TRUE ~ "bottom"),
-    depth_category = factor(depth_category,
-                            levels = c("surface", "bottom")),
-    season = case_when(dateTime > "2016-09-01" ~ "fall",
-                       TRUE                      ~ "summer"),
-    season = factor(season,
-                    levels = c("summer", "fall"))
+                               depth == 4.5 | depth == 6.5 ~ "bottom",
+                               depth %in% c("2","4","4") ~ depth,
+                               TRUE ~ "other")
+    # depth_category = factor(depth_category,
+    #                         levels = c("surface", "bottom"))
   ) 
 
 #' Meterological data
@@ -179,6 +177,79 @@ wx <- read_csv(here("data/WY2016to2022_LochVale_WX.csv")) %>%
 chl <- read_csv(here("data/AllPelagicCHLA_2016-2021.csv")) %>%
   mutate(date= mdy(date))
 
+
+
+
+#' Does the timeseries look ok? 
+sky_buoy_long %>%
+  filter(depth %in% c("0.5","6.5")) %>%
+  ggplot(aes(x=dateTime, y=value, color=factor(depth))) +
+  geom_point(alpha=0.2)+
+  facet_wrap(parameter~., nrow=2, scales="free_y")+
+  scale_x_datetime(date_breaks = "2 months",
+               date_minor_breaks = "4 weeks")
+#' Some kind of crazy dynamics going on around ice-off.
+#' Interesting that winter 2016-2017 the lake pretty distinctly reverse stratifes, but not in 2017-2018
+#' in 2017, The Loch (subalpine lakes ~200m lower in elevation) had ice-off by 5/16. 
+#' Usually Sky Pond is ~2 weeks or a little more behind.
+
+sky_buoy_long %>%
+  filter(depth %in% c("0.5","6.5")) %>%
+  filter(dateTime >= "2017-05-01" & dateTime <= "2017-06-01") %>%
+  ggplot(aes(x=dateTime, y=value, color=factor(depth))) +
+  geom_point(alpha=0.2)+
+  facet_wrap(parameter~., nrow=2, scales="free_y")+
+  scale_x_datetime(date_breaks = "3 days",
+                   date_minor_breaks = "1 day")+
+  labs(title="Around ice-off 2017")
+
+sky_buoy_long %>%
+  filter(depth %in% c("0.5","6.5")) %>%
+  filter(dateTime >= "2018-05-01" & dateTime <= "2018-06-01") %>%
+  ggplot(aes(x=dateTime, y=value, color=factor(depth))) +
+  geom_point(alpha=0.2)+
+  facet_wrap(parameter~., nrow=2, scales="free_y")+
+  scale_x_datetime(date_breaks = "3 days",
+                   date_minor_breaks = "1 day")+
+  labs(title="Around ice-off 2018")
+
+
+
+
+#' Does the timeseries look ok? 
+loch_buoy_long %>%
+  filter(depth %in% c("0.5","4.5")) %>%
+  ggplot(aes(x=dateTime, y=value, color=factor(depth))) +
+  geom_point(alpha=0.2)+
+  facet_wrap(parameter~., nrow=2, scales="free_y")+
+  scale_x_datetime(date_breaks = "2 months",
+                   date_minor_breaks = "4 weeks")
+#' Some kind of crazy dynamics going on around ice-off.
+#' Interesting that winter 2016-2017 the lake pretty distinctly inverse stratifes, but not in 2017-2018
+#' in 2017, The Loch  had ice-off by 5/16. 
+
+loch_buoy_long %>%
+  filter(depth %in% c("0.5","4.5")) %>%
+  filter(dateTime >= "2017-05-01" & dateTime <= "2017-06-01") %>%
+  ggplot(aes(x=dateTime, y=value, color=factor(depth))) +
+  geom_point(alpha=0.2)+
+  facet_wrap(parameter~., nrow=2, scales="free_y")+
+  scale_x_datetime(date_breaks = "3 days",
+                   date_minor_breaks = "1 day")+
+  labs(title="Around ice-off 2017")
+
+#' Some wild DO dynamics in late april early may ahead of ice-out. 
+loch_buoy_long %>%
+  filter(depth %in% c("0.5","4.5")) %>%
+  filter(dateTime >= "2018-04-15" & dateTime <= "2018-05-15") %>%
+  ggplot(aes(x=dateTime, y=value, color=factor(depth))) +
+  geom_point(alpha=0.2)+
+  facet_wrap(parameter~., nrow=2, scales="free_y")+
+  scale_x_datetime(date_breaks = "3 days",
+                   date_minor_breaks = "1 day")+
+  labs(title="Around ice-off 2018")
+
+
 #' # Data vis: D.O. from buoys ----------------------------------------------------------------
 
 
@@ -186,10 +257,10 @@ chl <- read_csv(here("data/AllPelagicCHLA_2016-2021.csv")) %>%
 #+ fig.height=10, fig.width=15, echo=FALSE, warning=FALSE, message=FALSE
 buoy_master %>%
   filter(parameter=="DO")%>%
-  ggplot(aes(x=dateTime,y=value,color=depth_category))+
+  ggplot(aes(x=dateTime,y=value,color=depth_category,shape=season))+
   geom_point(alpha=0.5,size=0.2)+
   geom_line(alpha=0.5)+
-  facet_wrap(lakeID~season, scales="free_x") +
+  facet_wrap(lakeID~year, scales="free_x") +
   labs(title="Dissolved oxygen",
        y="D.O. (mg/L)")+
   theme(legend.position="bottom")
